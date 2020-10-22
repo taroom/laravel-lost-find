b159c0af5e555cf736619e3994a4746f226fea97
sultanroomcode <sultanroomcode@gmail.com>
2020-10-22 23:06:34 +0700

    implementasi template enno

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,16 +8,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('enno/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('enno/assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('enno/assets/vendor/icofont/icofont.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('enno/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('enno/assets/vendor/venobox/venobox.css') }}" rel="stylesheet">
+    <link href="{{ asset('enno/assets/vendor/owl.carousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -76,5 +73,16 @@
             @yield('content')
         </main>
     </div>
+    <!-- Scripts -->
+    <script src="{{ asset('enno/assets/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('enno/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('enno/assets/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('enno/assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('enno/assets/vendor/waypoints/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('enno/assets/vendor/counterup/counterup.min.js') }}"></script>
+    <script src="{{ asset('enno/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('enno/assets/vendor/venobox/venobox.min.js') }}"></script>
+    <script src="{{ asset('enno/assets/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('enno/assets/js/main.js') }}" defer></script>
 </body>
 </html>
