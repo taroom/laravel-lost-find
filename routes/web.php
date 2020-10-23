e0af7ce1922f05395fdbe966dfaf3964aa9b30fe
sultanroomcode <sultanroomcode@gmail.com>
2020-10-24 05:08:39 +0700

    remove double route

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,6 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('barang', 'BarangController');
